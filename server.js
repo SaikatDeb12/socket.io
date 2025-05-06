ce17fe93308c78b2373b56b3921b15bb36f17eb9
SaikatDeb12 <saikat.rcld@gmail.com>
2025-05-06 13:34:03 +0530

    basic chat app

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 io.on("connection", (socket) => {
   console.log("User connected!"),
-    socket.on("disconnect", () => console.log("User disconnected!"));
+    socket.on("chat message", (msg) => {
+      console.log("Message: ", msg);
+      //broadcast
+      io.emit("chat message", msg);
+    });
+
+  socket.on("disconnect", () => console.log("User disconnected!"));
 });
 
 server.listen(8000, () => console.log("server started at port: 8000"));
