cffefdeb7a55f655cff359f524486b2be485b30f
SaikatDeb12 <saikat.rcld@gmail.com>
2025-05-06 13:00:02 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected!"),
+    socket.on("disconnect", () => console.log("User disconnected!"));
+});
+
+server.listen(8000, () => console.log("server started at port: 8000"));
